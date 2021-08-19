e26b3307768d714965af9aaf94c049bcae5f8e8f
Nicolas NM. METIVIER <nicolas.metivier@nirah-technology.fr>
2021-08-19 22:26:47 +0200

    Add form

--- src/pages/profilePage.jsx
+++ src/pages/profilePage.jsx
@@ -1,23 +1,20 @@
 import React, {
   useState,
-  useRef,
 } from 'react';
 import { withRouter } from 'react-router';
 import { TabView, TabPanel } from 'primereact/tabview';
-import { Toast } from 'primereact/toast';
+import { Button } from 'primereact/button';
 
 function ProfilePage() {
-  const toast = useRef(null);
   const [activeIndex, setActiveIndex] = useState(0);
   return (
     <section className="Page Page-Profile">
-      <Toast ref={toast} />
       <TabView activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}>
         <TabPanel header="Pilot" leftIcon="pi pi-user">
           Content I
         </TabPanel>
         <TabPanel header="Garage" leftIcon="pi pi-cog">
-          Content II
+          <Button icon="pi pi-plus" label="Add new motorbike" />
         </TabPanel>
       </TabView>
     </section>
