6cb06825ddf9e68ed93287606b87fba0c3892c0d
NicolasMetivier-at-162491939266 <metivier.nicolas@engie.com>
2021-08-24 22:07:47 +0200

    [PRODUCTION-SETUP] Clean project for production.

--- production-builder.py
+++ production-builder.py
@@ -3,10 +3,13 @@
 import os;
 
 def main():
-    os.system("git add .")
+    print("SAVING DEV...");
+    os.system("git add .");
     os.system('git commit -m "[PRODUCTION-SETUP] Clean project for production."');
     os.system("git push --set-upstream origin dev");
+    print("SAVING MAIN...");
     os.system("git merge main");
+    input();
     os.remove(".eslintrc.js");
     os.system("git add .")
     os.system('git commit -m "[PRODUCTION-SETUP] Clean project for production."');
