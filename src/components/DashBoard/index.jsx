50ad4e3f6dfc0ad78deb12c7ed757378bdc0ad6b
NicolasMetivier-at-162491939266 <metivier.nicolas@engie.com>
2021-08-25 13:16:40 +0200

    Implements DASHBOARD

--- src/components/DashBoard/index.jsx
+++ src/components/DashBoard/index.jsx
@@ -2,13 +2,25 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { withRouter } from 'react-router';
+import { Card } from 'primereact/card';
+import { Divider } from 'primereact/divider';
 
 import './style.css';
 
 function DashBoard() {
   return (
     <section className="Component Component-DashBoard">
-      oo
+      <Card title="Created Road Trips">
+        You are not planned any road trip.
+      </Card>
+      <Divider />
+      <Card title="Your Next Road Trips">
+        You are not yet registered on any road trip.
+      </Card>
+      <Divider />
+      <Card title="Your Previous Road Trips">
+        You have not yet accomplished any road trip.
+      </Card>
     </section>
   );
 }
